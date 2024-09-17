3d6080dae51570271417794cfc5295ef0a041ee5
dallinjackson <dallin.e.jackson@gmail.com>
2024-09-16 19:48:46 -0600

    King moves pass

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,9 +1,6 @@
 package chess;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 
 /**
@@ -48,8 +45,6 @@ public class PieceMovesCalculator {
         return calculator.pieceMoves(board, myPosition);
     }
 
-
-
     public enum Directions {
         UP,
         UPRIGHT,
@@ -60,6 +55,129 @@ public class PieceMovesCalculator {
         LEFT,
         UPLEFT,
     }
+
+    /**
+     * This takes the initial direction and checks until false is returned, adding all valid moves while true
+     * @Params
+     * */
+    protected Boolean recursiveCheck(ChessBoard board, ChessPosition startPosition, Directions direction) {
+        // Check for edge of board
+        ChessPosition positionToCheck = checkEdgeOfBoard(startPosition, direction);
+        if (positionToCheck == null) {
+            return Boolean.FALSE;
+        }
+        //Get Piece at start and position to check
+        ChessPiece movingPiece = board.getPiece(this.currentPosition);
+        ChessPiece pieceToCheck = board.getPiece(positionToCheck);
+
+        int squareCheck = squareChecker(movingPiece.getTeamColor(), pieceToCheck);
+        // Handle squareCheckReturn
+        switch (squareCheck) {
+            case 0 -> {
+                addMove(positionToCheck, null);
+                return recursiveCheck(board, positionToCheck, direction); // Recursion
+            }
+            case 1 -> {
+                addMove(positionToCheck, null); // Enemy position, count the move as a possible attack then
+                // return true. Don't keep recursing
+                return Boolean.TRUE;
+            }
+            case 2 -> {
+                // invalid, don't continue or add move.
+                return Boolean.FALSE;
+            }
+            default -> throw new IllegalArgumentException("Incorrect checker function called in PMC.");
+        }
+    }
+
+
+    protected int singleCheck(ChessBoard board, ChessPosition startPosition, Directions direction) {
+        // Check for edge of board
+        ChessPosition positionToCheck = checkEdgeOfBoard(startPosition, direction);
+        if (positionToCheck == null) {
+            return 2; //Edge
+        }
+        //Get Piece at start and position to check
+        ChessPiece movingPiece = board.getPiece(this.currentPosition);
+        ChessPiece pieceToCheck = board.getPiece(positionToCheck);
+        // Check
+        return squareChecker(movingPiece.getTeamColor(), pieceToCheck);
+
+    }
+
+    protected void pawnMovements(ChessBoard board, ChessPosition myPosition, Set<ChessPiece.PieceType> promotionTypes) {
+        ChessPiece pawnObject = board.getPiece(myPosition);
+        boolean isWhite = pawnObject.getTeamColor() == ChessGame.TeamColor.WHITE;
+        int direction = isWhite ? 1 : -1;
+        boolean firstMove = (isWhite && myPosition.getRow() == 2) || (!isWhite && myPosition.getRow() == 7);
+
+        Directions forward = isWhite ? Directions.UP : Directions.DOWN;
+        Directions left = isWhite ? Directions.UPLEFT : Directions.DOWNLEFT;
+        Directions right = isWhite ? Directions.UPRIGHT : Directions.DOWNRIGHT;
+
+        if (singleCheck(board, myPosition, forward) == 0) {
+            ChessPosition advanceOne = new ChessPosition(myPosition.getRow() + direction, myPosition.getColumn());
+            addPawnMove(advanceOne, pawnObject.getTeamColor(), promotionTypes);
+
+            if (firstMove && singleCheck(board, advanceOne, forward) == 0) {
+                addMove(new ChessPosition(advanceOne.getRow() + direction, advanceOne.getColumn()), null);
+            }
+        }
+        if (singleCheck(board, myPosition, left) == 1) {
+            ChessPosition attackLeft = new ChessPosition(myPosition.getRow() + direction, myPosition.getColumn() - 1);
+            addPawnMove(attackLeft, pawnObject.getTeamColor(), promotionTypes);
+        }
+        if (singleCheck(board, myPosition, right) == 1) {
+            ChessPosition attackRight = new ChessPosition(myPosition.getRow() + direction, myPosition.getColumn() + 1);
+            addPawnMove(attackRight, pawnObject.getTeamColor(), promotionTypes);
+        }
+    }
+
+    private void addPawnMove(ChessPosition newPosition, ChessGame.TeamColor color, Set<ChessPiece.PieceType> promotionTypes) {
+        if ((newPosition.getRow() == 1) && color == ChessGame.TeamColor.BLACK) {
+            for (ChessPiece.PieceType type : promotionTypes) {
+                addMove(newPosition, type);
+            }
+        } else if ((newPosition.getRow() == 8) && color == ChessGame.TeamColor.WHITE) {
+            for (ChessPiece.PieceType type : promotionTypes) {
+                addMove(newPosition, type);
+            }
+        } else {
+            addMove(newPosition, null);
+        }
+    }
+
+    protected ChessPosition checkEdgeOfBoard(ChessPosition startPosition, Directions direction) {
+        int row = startPosition.getRow();
+        int col = startPosition.getColumn();
+
+        return switch (direction) {
+            case UP -> (row < 8) ? new ChessPosition(row + 1, col) : null;
+            case DOWN -> (row > 1) ? new ChessPosition(row - 1, col) : null;
+            case LEFT -> (col > 1) ? new ChessPosition(row, col - 1) : null;
+            case RIGHT -> (col < 8) ? new ChessPosition(row, col + 1) : null;
+            case UPRIGHT -> (row < 8 && col < 8) ? new ChessPosition(row + 1, col + 1) : null;
+            case UPLEFT -> (row < 8 && col > 1) ? new ChessPosition(row + 1, col - 1) : null;
+            case DOWNRIGHT -> (row > 1 && col < 8) ? new ChessPosition(row - 1, col + 1) : null;
+            case DOWNLEFT -> (row > 1 && col > 1) ? new ChessPosition(row - 1, col - 1) : null;
+        };
+    }
+
+    protected int squareChecker(ChessGame.TeamColor movingPieceColor, ChessPiece pieceAtSpot) {
+        if (pieceAtSpot == null) {
+            return 0; // Empty spot
+        } else if (pieceAtSpot.getTeamColor() != movingPieceColor) {
+            return 1; //Enemy spot
+        } else {
+            return 2; // Teammate in way/Edge Of Board
+        }
+    }
+
+    private void addMove(ChessPosition end, ChessPiece.PieceType promotion) {
+        ChessMove newMove = new ChessMove(this.currentPosition, end, promotion);
+        this.validMoves.add(newMove);
+    }
+
 
     //
     // ______________________________ Inner classes for Calculating ______________________________
@@ -84,28 +202,30 @@ public class PieceMovesCalculator {
             pawnMovements(board, myPosition, promotionTypes);
             return this.validMoves;
         }
-
-        //
-        //__________PAWN HELPERS__________
-        //
-
-
-
     }
 
     private static class KingMovesCalculator extends PieceMovesCalculator {
-        // Implementation for King moves
-        private Collection<ChessMove> validMoves;
-
+        protected KingMovesCalculator() {
+            this.validMoves = new ArrayList<>();
+        }
         @Override
         public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-
+            this.currentPosition = myPosition;
+            for (Directions direction : Directions.values()) {
+                ChessPosition position = checkEdgeOfBoard(myPosition, direction);
+                int checkResult = singleCheck(board, myPosition, direction);
+                if (checkResult == 0 || checkResult == 1) {
+                    this.validMoves.add(new ChessMove(myPosition, position, null));
+                }
+            }
             return this.validMoves;
         }
     }
 
     private static class KnightMovesCalculator extends PieceMovesCalculator {
-        private Collection<ChessMove> validMoves;
+        protected KnightMovesCalculator() {
+            this.validMoves = new ArrayList<>();
+        }
 
         @Override
         public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
@@ -170,191 +290,4 @@ public class PieceMovesCalculator {
 
 
 
-    //
-    // ______________________________ Helper Functions Inheritance ______________________________
-    //
-    /**
-     * This takes the initial direction and checks until false is returned, adding all valid moves while true
-     * @Params
-     * */
-    protected Boolean recursiveCheck(ChessBoard board, ChessPosition startPosition, Directions direction) {
-        // Check for edge of board
-        ChessPosition positionToCheck = checkEdgeOfBoard(startPosition, direction);
-        if (positionToCheck == null) {
-            return Boolean.FALSE;
-        }
-        //Get Piece at start and position to check
-        ChessPiece movingPiece = board.getPiece(this.currentPosition);
-        ChessPiece pieceToCheck = board.getPiece(positionToCheck);
-
-        int squareCheck = squareChecker(movingPiece.getTeamColor(), pieceToCheck);
-        // Handle squareCheckReturn
-        switch (squareCheck) {
-            case 0 -> {
-                addMove(positionToCheck, null);
-                return recursiveCheck(board, positionToCheck, direction); // Recursion
-            }
-            case 1 -> {
-                addMove(positionToCheck, null); // Enemy position, count the move as a possible attack then
-                // return true. Don't keep recursing
-                return Boolean.TRUE;
-            }
-            case 2 -> {
-                // invalid, don't continue or add move.
-                return Boolean.FALSE;
-            }
-            default -> throw new IllegalArgumentException("Incorrect checker function called in PMC.");
-        }
     }
-
-
-    protected int singleCheck(ChessBoard board, ChessPosition startPosition, Directions direction) {
-        // Check for edge of board
-        ChessPosition positionToCheck = checkEdgeOfBoard(startPosition, direction);
-        if (positionToCheck == null) {
-            return 2; //Edge
-        }
-        //Get Piece at start and position to check
-        ChessPiece movingPiece = board.getPiece(this.currentPosition);
-        ChessPiece pieceToCheck = board.getPiece(positionToCheck);
-        // Check
-        return squareChecker(movingPiece.getTeamColor(), pieceToCheck);
-
-    }
-
-    protected void pawnMovements(ChessBoard board, ChessPosition myPosition, Set<ChessPiece.PieceType> promotionTypes) {
-        Boolean firstMove;
-        ChessPiece pawnObject = board.getPiece(myPosition);
-
-        if (pawnObject.getTeamColor() == ChessGame.TeamColor.WHITE) {
-            if (myPosition.getRow() == 2) {
-                firstMove = Boolean.TRUE;
-            } else {
-                firstMove = Boolean.FALSE;
-            }
-            if (singleCheck(board, myPosition, Directions.UP) == 0) {
-                ChessPosition advanceOne = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn());
-
-                addPawnMove(advanceOne, ChessGame.TeamColor.WHITE, promotionTypes);
-                //if first move go forward again
-
-                if (Boolean.TRUE.equals(firstMove) && singleCheck(board, advanceOne, Directions.UP) == 0) {
-                    addMove(new ChessPosition(advanceOne.getRow() + 1, advanceOne.getColumn()), null);
-                }
-
-            }
-            if (singleCheck(board, myPosition, Directions.UPLEFT) == 1) {
-                ChessPosition attackLeft = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 1);
-                addPawnMove(attackLeft, ChessGame.TeamColor.WHITE, promotionTypes);
-            }
-            if (singleCheck(board, myPosition, Directions.UPRIGHT) == 1) {
-                ChessPosition attackRight = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 1);
-                addPawnMove(attackRight, ChessGame.TeamColor.WHITE, promotionTypes);
-
-            }
-
-        } else if (pawnObject.getTeamColor() == ChessGame.TeamColor.BLACK) {
-            if (myPosition.getRow() == 7) {
-                firstMove = Boolean.TRUE;
-            } else {
-                firstMove = Boolean.FALSE;
-            }
-            if (singleCheck(board, myPosition, Directions.DOWN) == 0) {
-
-                ChessPosition advanceOne = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn());
-
-                addPawnMove(advanceOne, ChessGame.TeamColor.BLACK, promotionTypes);
-
-                //if first move go forward again
-                if (Boolean.TRUE.equals(firstMove) && singleCheck(board, advanceOne, Directions.DOWN) == 0) {
-                    addMove(new ChessPosition(advanceOne.getRow() - 1, advanceOne.getColumn()), null);
-                }
-            }
-            if (singleCheck(board, myPosition, Directions.DOWNLEFT) == 1) {
-                ChessPosition attackLeft = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 1);
-                addPawnMove(attackLeft, ChessGame.TeamColor.BLACK, promotionTypes);
-            }
-            if (singleCheck(board, myPosition, Directions.DOWNRIGHT) == 1) {
-                ChessPosition attackLeft = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 1);
-                addPawnMove(attackLeft, ChessGame.TeamColor.BLACK, promotionTypes);
-            }
-        }
-    }
-
-    private void addPawnMove(ChessPosition newPosition, ChessGame.TeamColor color, Set<ChessPiece.PieceType> promotionTypes) {
-        if ((newPosition.getRow() == 1) && color == ChessGame.TeamColor.BLACK) {
-            for (ChessPiece.PieceType type : promotionTypes) {
-                addMove(newPosition, type);
-            }
-        } else if ((newPosition.getRow() == 8) && color == ChessGame.TeamColor.WHITE) {
-            for (ChessPiece.PieceType type : promotionTypes) {
-                addMove(newPosition, type);
-            }
-        } else {
-            addMove(newPosition, null);
-        }
-    }
-
-    protected ChessPosition checkEdgeOfBoard(ChessPosition startPosition, Directions direction) {
-        ChessPosition positionToCheck;
-        switch (direction) {
-            case UP -> {
-                if (startPosition.getRow() < 8) {
-                    positionToCheck = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn());
-                } else { return null; }
-            }
-            case DOWN -> {
-                if (startPosition.getRow() > 1) {
-                    positionToCheck = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn());
-                }else { return null; }
-            }
-            case LEFT -> {
-                if (startPosition.getColumn() > 1) {
-                    positionToCheck = new ChessPosition(startPosition.getRow(), startPosition.getColumn() - 1);
-                }else { return null; }
-            }
-            case RIGHT -> {
-                if (startPosition.getColumn() < 8) {
-                    positionToCheck = new ChessPosition(startPosition.getRow(), startPosition.getColumn() + 1);
-                }else { return null; }
-            }
-            case UPRIGHT -> {
-                if (startPosition.getRow() < 8 && startPosition.getColumn() < 8) {
-                    positionToCheck = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() + 1);
-                }else { return null; }
-            }
-            case UPLEFT -> {
-                if (startPosition.getRow() < 8 && startPosition.getColumn() > 1) {
-                    positionToCheck = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() - 1);
-                }else { return null; }
-            }
-            case DOWNRIGHT -> {
-                if (startPosition.getRow() > 1 && startPosition.getColumn() < 8) {
-                    positionToCheck = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() + 1);
-                }else { return null; }
-            }
-            case DOWNLEFT -> {
-                if ((startPosition.getRow() > 1) && (startPosition.getColumn() > 1)) {
-                    positionToCheck = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() - 1);
-                } else { return null; }
-            }
-            default -> throw new IllegalArgumentException("Invalid direction passed to PMC: " + direction);
-        }
-        return positionToCheck;
-    }
-
-    protected int squareChecker(ChessGame.TeamColor movingPieceColor, ChessPiece pieceAtSpot) {
-        if (pieceAtSpot == null) {
-            return 0; // Empty spot
-        } else if (pieceAtSpot.getTeamColor() != movingPieceColor) {
-            return 1; //Enemy spot
-        } else {
-            return 2; // Teammate in way/Edge Of Board
-        }
-    }
-
-    private void addMove(ChessPosition end, ChessPiece.PieceType promotion) {
-        ChessMove newMove = new ChessMove(this.currentPosition, end, promotion);
-        this.validMoves.add(newMove);
-    }
-}
