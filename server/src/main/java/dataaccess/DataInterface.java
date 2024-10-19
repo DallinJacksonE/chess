d59ea7f54e276f4fc12f64ce7ec4e0fa42cf3eae
dallinjackson <dallin.e.jackson@gmail.com>
2024-10-19 14:01:19 -0600

    created data interface and functionality

--- server/src/main/java/dataaccess/DataInterface.java
+++ server/src/main/java/dataaccess/DataInterface.java
@@ -2,5 +2,14 @@ package dataaccess;
 
 public interface DataInterface {
 
-
+    void clear() throws DataAccessException;
+    void createUser() throws DataAccessException;
+    void getUser() throws DataAccessException;
+    void createGame() throws DataAccessException;
+    void getGame() throws DataAccessException;
+    void listGames() throws DataAccessException;
+    void updateGame() throws DataAccessException;
+    void createAuth() throws DataAccessException;
+    void getAuth() throws DataAccessException;
+    void deleteAuth() throws DataAccessException;
 }
