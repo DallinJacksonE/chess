62969fec4efc5ded3194cfb361ad5b96aebc5d0f
dallinjackson <dallin.e.jackson@gmail.com>
2024-10-19 14:02:37 -0600

    functionality from interface added to SQL and Simple memory classes

--- server/src/main/java/dataaccess/SimpleLocalDataBase.java
+++ server/src/main/java/dataaccess/SimpleLocalDataBase.java
@@ -1,4 +1,53 @@
 package dataaccess;
 
 public class SimpleLocalDataBase implements DataInterface {
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public void createUser() throws DataAccessException {
+
+    }
+
+    @Override
+    public void getUser() throws DataAccessException {
+
+    }
+
+    @Override
+    public void createGame() throws DataAccessException {
+
+    }
+
+    @Override
+    public void getGame() throws DataAccessException {
+
+    }
+
+    @Override
+    public void listGames() throws DataAccessException {
+
+    }
+
+    @Override
+    public void updateGame() throws DataAccessException {
+
+    }
+
+    @Override
+    public void createAuth() throws DataAccessException {
+
+    }
+
+    @Override
+    public void getAuth() throws DataAccessException {
+
+    }
+
+    @Override
+    public void deleteAuth() throws DataAccessException {
+
+    }
 }
