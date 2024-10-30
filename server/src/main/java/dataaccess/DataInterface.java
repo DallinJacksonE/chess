b6f8eb66d037641f7209ce6d189ea90b56fabb65
dallinjackson <dallin.e.jackson@gmail.com>
2024-10-30 14:47:32 -0600

    added functionality for mysql

--- server/src/main/java/dataaccess/DataInterface.java
+++ server/src/main/java/dataaccess/DataInterface.java
@@ -3,8 +3,6 @@ package dataaccess;
 import model.*;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public interface DataInterface {
 
---
+++ server/src/main/java/dataaccess/MySQLDataBase.java
@@ -0,0 +1,96 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.ArrayList;
+
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class MySQLDataBase implements DataInterface {
+
+    public MySQLDataBase() throws DataAccessException {
+        configureDatabase();
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String userName) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Integer createGame(GameData gameData) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<GameData> listGames() throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData newGameData) throws DataAccessException {
+
+    }
+
+    @Override
+    public void createAuth(String userData, AuthData authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+}
