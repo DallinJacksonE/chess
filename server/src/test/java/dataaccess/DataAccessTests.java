9c96d048d8e92bb522f06e0976b13f35e3475c0c
dallinjackson <dallin.e.jackson@gmail.com>
2024-11-01 21:25:10 -0600

    polishing tests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,27 +1,163 @@
-package dataaccess;
+package service;
 
+import chess.exception.ResponseException;
+import dataaccess.DataAccessException;
+import dataaccess.DataInterface;
+import dataaccess.MySQLDataBase;
+import dataaccess.SimpleLocalDataBase;
+import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import service.Service;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import javax.xml.crypto.Data;
 
-public class DataAccessTests {
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataAccessTests {
     private DataInterface db;
     private Service service;
 
     @BeforeEach
-    public void setUp() {
-        db = new SimpleLocalDataBase();
+    public void setUp() throws DataAccessException {
+        db = new MySQLDataBase();
         service = new Service(db);
 
     }
 
+    @AfterEach
+    public void takeDown() throws DataAccessException {
+        db.clear();
+    }
+
     @Test
-    void addUserTest() {
-        
+    void registerNewUserTest() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        String token = service.register(userData);
+        assertNotNull(token);
+        assertEquals(userData.username(), db.getUser(userData.username()).username());
+    }
+
+    @Test
+    void registerUserTwice() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        String token = service.register(userData);
+        assertNotNull(token);
+        assertEquals(userData.username(), db.getUser(userData.username()).username());
+        assertThrows(ResponseException.class, () -> service.register(userData));
+    }
+
+    @Test
+    void clearTest() throws ResponseException {
+        service.clear();
+
+    }
+
+    @Test
+    void loginTest() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        String token = service.register(userData);
+        String loginToken = service.login(userData.username(), userData.password());
+        assertNotEquals(token, loginToken);
+    }
+
+    @Test
+    void loginWithBadPassword() throws ResponseException {
+        registerNewUserTest();
+        assertThrows(ResponseException.class, () -> service.login("username", "TyPoS"));
+    }
+
+    @Test
+    void loginToEmptyDB() throws ResponseException {
+        service.clear();
+        assertThrows(ResponseException.class, () -> service.login("username", "password"));
+    }
+
+    @Test
+    void successfulLogout() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        service.register(userData);
+        var token = service.login(userData.username(), userData.password());
+        service.logout(token);
+        var newToken = service.login(userData.username(), userData.password());
+        assertNotEquals(token, newToken);
+    }
+
+    @Test
+    void getGames() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        service.register(userData);
+        var token = service.login(userData.username(), userData.password());
+        assertDoesNotThrow(() -> service.getGames(token));
+    }
+
+    @Test
+    void getGamesWithBadToken() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        service.register(userData);
+        var token = service.login(userData.username(), userData.password());
+        service.logout(token);
+        var newToken = service.login("username", "password");
+        service.getGames(newToken);
+        assertThrows(ResponseException.class, () -> service.getGames(token));
+    }
+
+    @Test
+    void makeGame() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        service.register(userData);
+        var token = service.login(userData.username(), userData.password());
+        int gameID = service.createGame(token, "newGame");
+    }
+
+    @Test
+    void makeGameWithEmptyName() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        service.register(userData);
+        var token = service.login(userData.username(), userData.password());
+        assertThrows(ResponseException.class, () -> service.createGame(token, ""));
+    }
+
+    @Test
+    void joinGame() throws ResponseException {
+        UserData userData = new UserData("username", "password", "email@example.com");
+        service.register(userData);
+        var token = service.login(userData.username(), userData.password());
+        int gameID = service.createGame(token, "newGame");
+
+        service.joinGame(token, "WHITE", gameID);
+
+        GameData gameData = db.getGame(gameID);
+
+        assertEquals(gameData.whiteUsername(), userData.username());
+
+    }
+
+    @Test
+    void joinGameAlreadyTaken() throws ResponseException {
+        UserData userData1 = new UserData("username1", "password1", "email1@example.com");
+        UserData userData2 = new UserData("username2", "password2", "email2@example.com");
+        UserData userData3 = new UserData("username3", "password3", "email3@example.com");
+        service.register(userData1);
+        service.register(userData2);
+        service.register(userData3);
+        var token1 = service.login(userData1.username(), userData1.password());
+        var token2 = service.login(userData2.username(), userData2.password());
+        var token3 = service.login(userData3.username(), userData3.password());
+        int gameID = service.createGame(token1, "newGame");
+
+        service.joinGame(token1, "WHITE", gameID);
+        service.joinGame(token2, "BLACK", gameID);
+
+        GameData gameData = db.getGame(gameID);
+
+        assertEquals(gameData.whiteUsername(), userData1.username());
+        assertEquals(gameData.blackUsername(), userData2.username());
+
+        assertThrows(ResponseException.class, () -> service.joinGame(token3, "WHITE", gameID));
+
     }
 
 }
