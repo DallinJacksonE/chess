8313a9f087a3a778bb3fb4a669502802ac94714a
dallinjackson <dallin.e.jackson@gmail.com>
2024-11-09 14:15:42 -0700

    started file structure

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,14 @@
+package ui;
+
+public class ChessClient {
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+
+    }
+
+    public String eval(String input) {
+
+    }
+}
